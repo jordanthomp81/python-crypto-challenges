59f213ceeb9d048d1956bbc9de94e73cc7438e80
Jordan Thompson <jordanthomp81@gmail.com>
2018-08-26 13:28:41 -0500

    added Fixed XOR

---
+++ fixed-xor.py
@@ -0,0 +1,6 @@
+# Fixed XOR
+# Write a function that takes two equal-length buffers and produces their XOR combination.
+
+def fixed_XOR(hexOne, hexTwo):
+    xor = int(hexOne, 16) ^ int(hexTwo, 16)
+    return '{:x}'.format(xored)
